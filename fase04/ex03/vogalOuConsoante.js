e8b1812312f4e897b473203f6ae4ee0ba62e1b6b
gabrielfontes7621 <49699332+gabrielfontes7621@users.noreply.github.com>
2019-05-07 20:51:39 -0300

    console.log(vogalOuConsoante("a"))

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,14 @@
+function vogalOuConsoante(letra) {
+ if (letra==="A")return "Vogal"
+ if (letra==="a")return "Vogal"
+ if (letra==="E")return "Vogal"
+ if (letra==="e")return "Vogal"
+ if (letra==="I")return "Vogal"
+ if (letra==="i")return "Vogal"
+ if (letra==="o")return "Vogal"
+ if (letra==="O")return "Vogal"
+ if (letra==="U")return "Vogal"
+ if (letra==="u")return "Vogal"
+ else 
+ return "Consoante"
+}
