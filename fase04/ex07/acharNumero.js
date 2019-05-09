c44e76969317c7e7e9c285b6a9118e991b36f3bc
gabrielfontes7621 <49699332+gabrielfontes7621@users.noreply.github.com>
2019-05-09 20:21:55 -0300

    console.log(acharNumero(1000))

--- fase04/ex07/acharNumero.js
+++ fase04/ex07/acharNumero.js
@@ -1,9 +1,9 @@
 function acharNumero(num) {
 if (num<5)
 return ("a")
-if (5<num && num<11)
+if (5<=num && num<=10)
 return ("b")
-if (10<num && num<101)
+if (10<num && num<=100)
 return ("c")
 else 
 return ("d")
