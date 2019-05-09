d8cecb055f9c14d9947369f79abd020ac5d68ed8
gabrielfontes7621 <49699332+gabrielfontes7621@users.noreply.github.com>
2019-05-09 19:33:58 -0300

    console.log(alfabetoMilitar("C"))

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,14 @@
+function alfabetoMilitar(letra) {
+if (letra==="A")return "alpha"
+ if (letra==="a")return "alpha"
+ if (letra==="E")return "echo"
+ if (letra==="e")return "echo"
+ if (letra==="I")return "india"
+ if (letra==="i")return "india"
+ if (letra==="o")return "oscar"
+ if (letra==="O")return "oscar"
+ if (letra==="U")return "uniform"
+ if (letra==="u")return "uniform"
+ else 
+ return "false"
+}
