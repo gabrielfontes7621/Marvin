7a54208eed2191002f74c1c997bb06b1550fdb57
gabrielfontes7621 <49699332+gabrielfontes7621@users.noreply.github.com>
2019-05-09 20:30:46 -0300

    Update abc.js

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -1,19 +1,10 @@
 function abc(letra) {
  if (letra.substring(0,1)=== "a") return true
- else
-return false;
   if (letra.substring(0,1)=== "A") return true
-  else
-return false;
    if (letra.substring(0,1)=== "B") return true
-   else
-return false ;
     if (letra.substring(0,1)=== "b") return true
-    else
-return false;
+
      if (letra.substring(0,1)=== "C") return true
-     else
-return false;
       if (letra.substring(0,1)=== "c") 
 return true
 else
