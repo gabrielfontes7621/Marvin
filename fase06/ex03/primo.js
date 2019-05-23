f8ff66de74c2e9f264fb941ff5331f4cb71d514b
gabrielfontes7621 <49699332+gabrielfontes7621@users.noreply.github.com>
2019-05-23 19:09:45 -0300

    console.log(primo(102))

--- fase06/ex03/primo.js
+++ fase06/ex03/primo.js
@@ -1,22 +1,8 @@
-function por42(num1,num2) {
-        var resp = false
-        
-        
-        
-        while (resp===false && num1<=num2){
-            if (num1%42===0){
-                resp=num1 + 42
-            }
-            num1++
-            
-            
-        }
-        if (resp>num2){
-         resp=false   
-        }
-        if (resp===false){
-         console.log("Não encontrado")   
-        }    
-        return resp
+function primo(x){
+    if (x=== 2|| x===3||x===5||x===7||x===11)
+    return "Sim"
+    if (x % 2 ===0 ||x % 3 ===0||x % 5 ===0||x % 7 ===0 ||x % 11===0)
+    return "Não"
+    else 
+    return "Sim"
 }
-
